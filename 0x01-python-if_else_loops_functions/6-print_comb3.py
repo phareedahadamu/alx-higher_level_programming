26eb68f65aaf661f2e775224eaf0581df3fb9025
phareedahadamu <phareedahadamu@gmail.com>
2023-11-28 18:02:24 +0100

    Task 6 rev

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for i in range(0, 10):
     for j in range(0, 10):
         if j > i:
-            print(f"{i}{j}", end="")
-            print(f", "if i < 8 else "\n", end="")
+            print("{}{}".format(i, j), end="")
+            print("{}".format(", ") if i < 8 else "{}".format("\n") , end="")
