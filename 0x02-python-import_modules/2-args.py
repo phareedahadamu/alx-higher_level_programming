9eedb264da344944d6949d815c05dd64cafb5079
phareedahadamu <phareedahadamu@gmail.com>
2023-11-30 15:53:14 +0100

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     if argc < 1:
         print("{} arguments.".format(argc))
     else:
-        print("{} argument:".format(argc) if argc == 1 else "{} arguments:".format(argc))
+        print("{} argument:".format(argc) if argc == 1
+              else "{} arguments:".format(argc))
         for i in range(1, argc + 1):
             print("{}: {}".format(i, sys.argv[i]))
