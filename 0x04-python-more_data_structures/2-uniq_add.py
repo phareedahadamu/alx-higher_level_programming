ff3009c0dddb76c5b09bcee245fa3a3449460280
phareedahadamu <phareedahadamu@gmail.com>
2023-12-24 23:48:09 +0100

    Task 2 rev

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,7 +4,7 @@ def uniq_add(my_list=[]):
     for i in my_list:
         if my_list.count(i) > 1:
             j = 1
-            while j < my_list.count(i):
+            while j <= my_list.count(i):
                 my_list.remove(i)
                 j = j + 1
     for k in my_list:
