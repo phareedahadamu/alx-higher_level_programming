e65227845229dfafc1010d88c0e23e769a145d6d
phareedahadamu <phareedahadamu@gmail.com>
2023-12-30 23:36:16 +0100

    Task 1..

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@ def safe_print_integer(value):
     check = 0
     try:
         print("{:d}".format(value))
-    except ValueError:
+    except (ValueError, TypeError):
         check = 1
     return True if check == 0 else False
