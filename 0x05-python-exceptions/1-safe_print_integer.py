24eb45cd60f415b432bdc8abf4912d7a656083a0
phareedahadamu <phareedahadamu@gmail.com>
2023-12-30 23:25:00 +0100

    Task 1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    check = 0
+    try:
+        print("{:d}".format(value))
+    except ValueError:
+        check = 1
+    return True if check == 0 else False
