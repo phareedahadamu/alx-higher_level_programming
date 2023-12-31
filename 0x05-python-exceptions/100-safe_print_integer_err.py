96c41106f63f414733d09eac77a2bbc88d24e54a
phareedahadamu <phareedahadamu@gmail.com>
2023-12-31 14:14:19 +0100

    Task 100..

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    import sys
     check = 0
     try:
         print("{:d}".format(value))
     except (ValueError, TypeError) as ex:
         check = 1
-        print("Exception: {}".format(ex))
+        print("Exception: {}".format(ex), file=sys.stderr)
     return True if check == 0 else False
