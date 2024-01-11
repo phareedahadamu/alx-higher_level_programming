62859ccbbf5bb0689d06531c74051cd64e95ade0
phareedahadamu <phareedahadamu@gmail.com>
2024-01-11 11:38:42 +0100

    Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Define a class square"""
+
+
+class Square:
+    """Instantiation"""
+    def __init__(self, size=0):
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
