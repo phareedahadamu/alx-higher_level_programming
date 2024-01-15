1302d6123457d205732e23b11019bd4e8a022a5c
phareedahadamu <phareedahadamu@gmail.com>
2024-01-15 13:51:26 +0100

    Task 6 finally

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""Define a class square"""
+
+"""Define a class Square."""
 
 
 class Square:
     """Instantiation"""
+
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -14,7 +16,7 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if isinstance(value, int) is False:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
@@ -33,10 +35,10 @@ class Square:
         self.__position = value
 
     def area(self):
-        return self.__size ** 2
+        return (self.__size ** 2)
 
     def my_print(self):
-        if self.size == 0:
+        if self.__size == 0:
             print()
         else:
             [print() for i in range(0, self.__position[1])]
