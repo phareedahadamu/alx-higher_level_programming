8a375b029d519944552bd522ef78b22cba962157
phareedahadamu <phareedahadamu@gmail.com>
2024-02-05 23:29:50 +0100

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+- a function that returns the list of available attributes
+and methods of an object
+- Returns a list object
+"""
+
+
+def lookup(obj):
+    return (dir(obj))
