2d667a0df8e8bf880c86cff4572654bf227701ff
phareedahadamu <phareedahadamu@gmail.com>
2024-02-07 02:14:55 +0100

    Task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Defines a class Square, that inherits from
+Rectangle that inherits from BaseGeometry"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Features of class square"""
+
+    def __init__(self, size):
+        """Instatiation of square"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """ String representation of Square"""
+        return ("[Square] {}/{}".format(self.__size, self.__size))
