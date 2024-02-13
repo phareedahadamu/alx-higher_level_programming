aa030b827883c6687226f2373c50fa9c4b1eed88
phareedahadamu <phareedahadamu@gmail.com>
2024-02-13 21:26:14 +0100

    Task 4..

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" """
+""" Defines from_json_string function""" 
 
 
 def from_json_string(my_str):
-    """ """
+    """ A function that returns an object (Python data structure) represented
+    by a JSON string"""
 
     import json
     return(json.loads(my_str))
