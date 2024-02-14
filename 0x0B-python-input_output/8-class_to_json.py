8e8670cc1ccc6ea1b1eefc95b70ca99eda6e787f
phareedahadamu <phareedahadamu@gmail.com>
2024-02-14 23:05:09 +0100

    Task 8 fix

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -7,15 +7,4 @@ def class_to_json(obj):
     structure (list, dictionary, string, integer and boolean) for JSON
     serialization of an object"""
 
-    import json
-    from json import JSONEncoder
-
-    class objEncoder(JSONEncoder):
-        """ Defines a class objEncoder that encodes the class
-        and converts to a dict"""
-        def default(self, my_obj):
-            """ converts to a dict"""
-            return my_obj.__dict__
-    objEncoder().encode(obj)
-    objJSON = json.dumps(obj, cls=objEncoder)
-    return json.loads(objJSON)
+    return (obj.__dict__)
