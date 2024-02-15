c97b3a5f1b685641415ce24ea9b2b5e020d8e752
phareedahadamu <phareedahadamu@gmail.com>
2024-02-16 00:12:23 +0100

    Task 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -36,7 +36,7 @@ class Rectangle(Base):
         if not (isinstance(value, int)):
             raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError("heigth must be > 0")
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
