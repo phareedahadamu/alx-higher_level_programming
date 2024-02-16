89b39ae7cf14ad2d6e5d0ad61d29e8f109be0734
phareedahadamu <phareedahadamu@gmail.com>
2024-02-16 17:40:38 +0100

    Task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,6 +11,15 @@ class Square(Rectangle):
         """Instantiation of Square"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """ String representation of Square when printed"""
         return "[Square] ({}) {}/{} - {}".format(self.id,
