da624bfca0f47871032d2ef0de181059b07429ab
phareedahadamu <phareedahadamu@gmail.com>
2024-02-16 16:33:59 +0100

    Task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Defines the class Square"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Features of class Square
+    Inherits from class Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Instantiation of Square"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ String representation of Square when printed"""
+        return "[Square] ({}) {}/{} - {}".format(self.id,
+                                                 self.x, self.y, self.width)
