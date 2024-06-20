9ce2665463963dc31dc3562ae35bf778c070c076
phareedahadamu <phareedahadamu@gmail.com>
2024-06-20 22:59:51 +0100

    Task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* prints My number: <first argument converted in integer> if the first argument can be converted to an integer:
+ * If the argument can’t be converted to an integer, print “Not a number”
+ * You must use console.log(...) to print all output
+ * You are not allowed to use var
+ * You are not allowed to use try/catch
+ */
+if (isNaN(Number(process.argv[2]))) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + process.argv[2]);
+}
