87d99a00aec8639721d7883e593978c8e2eb5d2e
phareedahadamu <phareedahadamu@gmail.com>
2024-06-20 20:49:01 +0100

    Task 4

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* prints two arguments passed to it, in the following format: “ is ”
+ * You must use console.log(...) to print all output
+ * You are not allowed to use var
+ */
+console.log(process.argv[2] + ' is ' + process.argv[3]);
